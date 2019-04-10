264534e3cab192c10218373316287048c2530e06
khanishvili <khnshvl@gmail.com>
2019-04-09 22:10:58 -0400

    Removed extra lines

--- CarGarage/Program.cs
+++ CarGarage/Program.cs
@@ -6,8 +6,6 @@ namespace CarGarage
     {
         static void Main(string[] args)
         {
-            //asd
-
 
             Console.WriteLine("\t\t\t==== Wellcome ====");
             Garage mycars = new Garage();
@@ -29,30 +27,31 @@ namespace CarGarage
                         break;
                     case (char)ConsoleKey.D2: //Select car
                         Console.WriteLine("Please Type ID (NUMBERS) only and Press Enter : ");
-                         int selector = Convert.ToInt32(Console.ReadLine());
-                        if (selector>mycars.MyCars.Count)
+                        int selector = Convert.ToInt32(Console.ReadLine());
+                        if (selector > mycars.MyCars.Count)
                         {
                             Console.WriteLine("We don't have a car with this ID number");
                         }
                         else
                         {
                             Test_Drive(mycars.SelectCar(selector));
-                            
+
                         }
                         break;
                     case (char)ConsoleKey.D3: //Add car
-                        Console.Write(" Make : "); string make = Console.ReadLine();
-                        Console.Write(" Model : "); string model = Console.ReadLine();
+                        Console.Write(" Make : ");
+                        string make = Console.ReadLine();
+                        Console.Write(" Model : ");
+                        string model = Console.ReadLine();
                         mycars.AddCar(make, model);
-                        Console.WriteLine("Car {0} {1} is parked in garage",make,model);
+                        Console.WriteLine("Car {0} {1} is parked in garage", make, model);
 
-                        
                         break;
                     case (char)ConsoleKey.D4: //Remove Car
                         Console.WriteLine("Please Type ID   and Press Enter  to remove car from list ");
                         int index = Convert.ToInt32(Console.ReadLine());
 
-                        mycars.RemoveCar(index); 
+                        mycars.RemoveCar(index);
                         Console.WriteLine("Press 1 to Print list");
                         break;
                     case (char)ConsoleKey.D5: //Fuell All Cars
@@ -69,25 +68,26 @@ namespace CarGarage
                 }
             }
             while (run);
-       }
+        }
 
         private static void Test_Drive(Car selectedcar)
         {
-          
-         Console.WriteLine("You selected to drive {0} {1} press 5 for instrusctions!" ,selectedcar.Make,selectedcar.Model);
-         string carisoffmessage = "Your car is off press 1 to start!!";
+
+            Console.WriteLine("You selected to drive {0} {1} press 5 for instrusctions!", selectedcar.Make, selectedcar.Model);
+            string carisoffmessage = "Your car is off press 1 to start!!";
             bool run = true;
-         do{
-                
+            do
+            {
+
                 var key = Console.ReadKey();
                 switch (key.KeyChar)
-                { 
-                case (char)ConsoleKey.D1: //Start or Stop
+                {
+                    case (char)ConsoleKey.D1: //Start or Stop
                         selectedcar.ToggleMethod();
-                    Console.WriteLine("");
-                    
-                    break;
-                case (char)ConsoleKey.D2: // Accelerate
+                        Console.WriteLine("");
+
+                        break;
+                    case (char)ConsoleKey.D2: // Accelerate
                         if (selectedcar.EngineStatus)
                         {
                             selectedcar.Accelerate();
@@ -98,22 +98,22 @@ namespace CarGarage
                             Console.WriteLine(carisoffmessage);
                         }
                         Console.WriteLine("press 0 to quit");
-                    break;
-                case (char)ConsoleKey.D3: // Brake
+                        break;
+                    case (char)ConsoleKey.D3: // Brake
                         if (selectedcar.EngineStatus)
                         {
-                            selectedcar.Brake ();
+                            selectedcar.Brake();
                             Console.WriteLine("You slowed down your car ");
                             Console.WriteLine("\t\t\t Speed {0}  {1}", selectedcar.Speed, selectedcar.GasLevel);
                         }
                         else
                         {
                             Console.WriteLine(carisoffmessage.ToString());
-                            
+
                         }
                         Console.WriteLine("press 0 to quit");
-                break;
-                case (char)ConsoleKey.D4: // Add Fuel
+                        break;
+                    case (char)ConsoleKey.D4: // Add Fuel
                         if (selectedcar.GasLevel == 100)
                         {
                             Console.WriteLine("\t\t\t Don't need fuel!!!!");
@@ -123,7 +123,7 @@ namespace CarGarage
                             selectedcar.AddFuel();
                             Console.WriteLine("\t\t\t You tank is full");
                         }
-                break;
+                        break;
                     case (char)ConsoleKey.D5:
                         Console.WriteLine("\t\t\t\t Press 1 to Start/Stop Your car");
                         Console.WriteLine("\t\t\t\t Press 2 to Accelerate");
@@ -132,15 +132,15 @@ namespace CarGarage
                         Console.WriteLine("\t\t\t\t Press 0 to quit");
                         break;
 
-                case (char)ConsoleKey.D0: //Select car
-                    run = false;
-                break;
-                default:
+                    case (char)ConsoleKey.D0: //Select car
+                        run = false;
+                        break;
+                    default:
                         Console.WriteLine("Press 5 for instructions");
-                    break;
+                        break;
+                }
             }
-        }
-            while(run);
+            while (run);
         }
 
         static void Header()
